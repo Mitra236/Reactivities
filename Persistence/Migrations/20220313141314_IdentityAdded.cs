58ff693ff7678b7f3c34fba2cfc1591b72459fdf
Mitra Pantic <m.pantic@vegait.rs>
2022-03-13 16:37:31 +0100

    Created User Identity

---
+++ Persistence/Migrations/20220313141314_IdentityAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class IdentityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DsiplayName",
+                table: "AspNetUsers",
+                newName: "DisplayName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                newName: "DsiplayName");
+        }
+    }
+}
