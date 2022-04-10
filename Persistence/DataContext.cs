db8ade26f9122cb72d2d2f60429ece266de21388
Mitra236 <mitra.pantic23@gmail.com>
2022-04-10 18:13:32 +0200

    Created Comments entity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAtendee> ActivityAtendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -29,6 +30,11 @@ namespace Persistence
                     .HasOne(u => u.Activity)
                     .WithMany(a => a.Atendees)
                     .HasForeignKey(aa => aa.ActivityId);
+            //delete activity delets a comments associated with it - cascade
+            builder.Entity<Comment>()
+                    .HasOne(a => a.Activity)
+                    .WithMany(c => c.Comments)
+                    .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
